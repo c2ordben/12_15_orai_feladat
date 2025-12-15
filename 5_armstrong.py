0c204d5bc70fa16b25084b20804bea9934d43d4b
c2ordben <c2ordben@vasvari.org>
2025-12-15 09:13:30 +0100

    5. kész

--- 5_armstrong.py
+++ 5_armstrong.py
@@ -1,18 +1,30 @@
 import unittest
 
-def armstrong(n:int)->bool:
+def armstrong(num:int)->bool:
     digits = str(num)
     n = len(digits)
     total = sum(int(digit) ** n for digit in digits)
-    return total == 
+    return total == num
 
 
 
 
 
 class TestFibo(unittest.TestCase):
+        def test_armstrong_true(self):
+            self.assertTrue(armstrong(153))
+            self.assertTrue(armstrong(9474))
+            self.assertTrue(armstrong(370))
+
+        def test_armstrong_false(self):
+            self.assertFalse(armstrong(100))
+            self.assertFalse(armstrong(200))
+            self.assertFalse(armstrong(123))
+
+        def test_single_digit(self):
+            for i in range(10):
+                self.assertTrue(armstrong(i))
 
 
-
-
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
