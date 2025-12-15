ad10cd6cb933cd03dad12d4389011f09fe5627e3
c2ordben <c2ordben@vasvari.org>
2025-12-15 09:09:19 +0100

    kezdés

---
+++ 5_armstrong.py
@@ -0,0 +1,18 @@
+import unittest
+
+def armstrong(n:int)->bool:
+    digits = str(num)
+    n = len(digits)
+    total = sum(int(digit) ** n for digit in digits)
+    return total == 
+
+
+
+
+
+class TestFibo(unittest.TestCase):
+
+
+
+
+unittest.main()
